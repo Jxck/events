76acafd2e9a29ba68044731e9e121f37104dca3e
Jxck <block.rxckin.beats@gmail.com>
2011-12-25 16:42:51 +0900

    add runtest.sh

---
+++ test/runtest.sh
@@ -0,0 +1,8 @@
+node test-event-emitter-add-listeners.js
+node test-event-emitter-check-listener-leaks.js
+node test-event-emitter-max-listeners.js
+node test-event-emitter-modify-in-emit.js
+node test-event-emitter-num-args.js
+node test-event-emitter-once.js
+node test-event-emitter-remove-all-listeners.js
+node test-event-emitter-remove-listeners.js
