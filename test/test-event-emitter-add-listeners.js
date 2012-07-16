2aed3155f15d22dc4ba878a466f28797f7f4a60d
Jxck <block.rxckin.beats@gmail.com>
2012-07-16 15:10:10 +0900

    update test for node v0.8.1

--- test/test-event-emitter-add-listeners.js
+++ test/test-event-emitter-add-listeners.js
@@ -31,6 +31,7 @@ if(typeof require === 'function') {
 var e = new events.EventEmitter();
 
 var events_new_listener_emited = [];
+var listeners_new_listener_emited = [];
 var times_hello_emited = 0;
 
 // sanity check
@@ -39,14 +40,19 @@ assert.equal(e.addListener, e.on);
 e.on('newListener', function(event, listener) {
   console.log('newListener: ' + event);
   events_new_listener_emited.push(event);
+  listeners_new_listener_emited.push(listener);
 });
 
-e.on('hello', function(a, b) {
+function hello(a, b) {
   console.log('hello');
   times_hello_emited += 1;
   assert.equal('a', a);
   assert.equal('b', b);
-});
+}
+e.on('hello', hello);
+
+var foo = function() {};
+e.once('foo', foo);
 
 console.log('start');
 
